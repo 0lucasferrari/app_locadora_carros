9cda70412322b807340936ac75cade26c58d7294
0lucasferrari <lucas.p.f.carvalho@gmail.com>
2023-02-12 10:46:21 -0300

    Adds and adjusts migration files

--- database/migrations/2023_02_11_115634_create_clientes_table.php
+++ database/migrations/2023_02_11_115634_create_clientes_table.php
@@ -15,9 +15,11 @@ class CreateClientesTable extends Migration
     {
         Schema::create('clientes', function (Blueprint $table) {
             $table->id();
+            $table->string('nome', 30);
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
