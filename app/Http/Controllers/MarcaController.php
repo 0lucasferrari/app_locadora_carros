f6d3a99e27bd50a701da077047630c4eb0ddff9c
0lucasferrari <lucas.p.f.carvalho@gmail.com>
2023-02-17 22:36:52 -0300

    Uses the php artisan storage:link command to create a symbolic link in the storage/app/imagens folder and adjusts the update function

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -109,11 +109,20 @@ class MarcaController extends Controller
                     $regrasDinamicas[$input] = $regra;
                 }
             }
-            return $request->validate($regrasDinamicas, $marca->feedback());
+            $request->validate($regrasDinamicas, $marca->feedback());
+        } else {
+            $request->validate($marca->rules(), $marca->feedback());
         }
 
-        $request->validate($marca->rules(), $marca->feedback());
-        $marca->update($request->all());
+
+        $image = $request->file('imagem');
+        $imagem_urn = $image->store('imagens', 'public');
+
+        $marca->update([
+            'nome' => $request->nome,
+            'imagem' => $imagem_urn
+        ]);
+
         return $marca;
     }
 
