92a57b49d3e6e08cbd7d6dab555ed2139bb0d267
0lucasferrari <lucas.p.f.carvalho@gmail.com>
2023-02-28 21:14:53 -0300

    Adds prefix v1 to routes

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['middleware' => 'jwt.auth'], function () {
+Route::prefix('v1')->middleware('jwt.auth')->group(function () {
     Route::apiResource('cliente', 'App\Http\Controllers\ClienteController');
     Route::apiResource('carro', 'App\Http\Controllers\CarroController');
     Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController');
